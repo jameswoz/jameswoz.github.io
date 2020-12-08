2f529f745d5b85b4453b2cb5c02c0a2a86bcb218
James Wozniak <wozniakjames2@gmail.com>
2020-12-08 09:11:57 -0600

    added new pages for each password and a way to reach these pages

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,10 +1,12 @@
+/* checks inputted password and takes user to appropiate page */
 function getPassword() {
     let inputVal = document.getElementById("password-field").value;
-    if(inputVal == "test") {
-        window.alert("test success");
-    }
+    if(inputVal == "test") { window.location.href = "/pages/k.html"; }
+    else if(inputVal == "test2") { document.location.href = 'http://jameswoz.github.io/k'; }
+    else if(inputVal == "test3") { document.location.href = 'http://jameswoz.github.io/k'; }
 }
 
+/* submits password when enter is hit */
 document.querySelector("submit-btn").addEventListener("keyup", event => {
     if(event.key !== "Enter") return;
     document.querySelector("submit-btn").click();
