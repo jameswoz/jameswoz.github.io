94d980e82f483b9b841b61a9ef0cb16ff1252cea
James Wozniak <wozniakjames2@gmail.com>
2020-12-09 16:18:13 -0600

    finished everything

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,9 +1,24 @@
 /* checks inputted password and takes user to appropiate page */
 function getPassword() {
     let inputVal = document.getElementById("password-field").value;
-    if(inputVal == "test") { window.location.href = "/pages/k.html"; }
-    else if(inputVal == "test2") { document.location.href = 'http://jameswoz.github.io/k'; }
-    else if(inputVal == "test3") { document.location.href = 'http://jameswoz.github.io/k'; }
+    if(inputVal == "test") { goK(); }
+    else if(inputVal == "test2") { goM(); }
+    else if(inputVal == "test3") { goT(); }
+}
+
+function goK() {
+    window.location.assign("/pages/k.html");
+    alert("");
+}
+
+function goM() {
+    window.location.assign("/pages/m.html");
+    alert("");
+}
+
+function goT() {
+    window.location.assign("/pages/t.html");
+    alert("");
 }
 
 /* submits password when enter is hit */
